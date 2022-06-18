213a8a94c33ca20d75ada9de0952b7b2f57a1baa
Brian Marete <bmarete10@gmail.com>
2022-06-18 16:13:55 +0300

    Add deployment screenshot

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
           await deleteLocalFiles([filteredpath]);
         }
       });
-    } catch (error: any) {
+    } catch (error) {
       return res.status(500).send(error.message);
     }
       
